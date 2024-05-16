15b1fa6b4b974f5b0797df05f5dd49d356f751a7
Jamil Husni Abdul Hanif <100312@corp.b2be.com>
2024-05-17 02:00:43 +0800

    bug fix - mobile view

--- app/modules/dskpn/Views/tp_maintenance.php
+++ app/modules/dskpn/Views/tp_maintenance.php
@@ -43,9 +43,7 @@
           </a>
         </li>
 
-        <div class="moving-tab position-absolute nav-link" style="padding: 0px; transition: all 0.5s ease 0s; transform: translate3d(0px, 0px, 0px); width: 1238px;" aria-selected="false" tabindex="-1" role="tab"><a class="nav-link mb-0 px-0 py-1 active" data-bs-toggle="tab" href="#cam1" role="tab" aria-controls="cam1" aria-selected="true">-</a></div>
-        <div class="moving-tab position-absolute nav-link" aria-selected="false" tabindex="-1" role="tab" style="padding: 0px; width: 1238px; transform: translate3d(0px, 0px, 0px); transition: all 0.5s ease 0s;"><a class="nav-link mb-0 px-0 py-1 active" data-bs-toggle="tab" href="#spesifikasi_pengajaran" role="tab" aria-controls="spesifikasi_pengajaran" aria-selected="true" tabindex="-1">-</a>
-        </div>
+        
       </ul>
     </div>
   </div>
@@ -68,7 +66,7 @@
 
         </div>
         <hr class="custom">
-        <div class="text-end p-2 pb-3">
+        <div class="d-flex justify-content-end p-2 pb-3">
             <button class="btn bg-gradient-secondary me-1 mt-2" onclick="clearDynamicInputs()">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                     <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
