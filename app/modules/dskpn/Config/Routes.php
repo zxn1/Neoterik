929cfd06ec0124d2b9092aa31f78a391f13a3738
Jamil Husni Abdul Hanif <100312@corp.b2be.com>
2024-05-17 09:52:34 +0800

    add syafiq stuff and fixing design

--- app/modules/dskpn/Config/Routes.php
+++ app/modules/dskpn/Config/Routes.php
@@ -8,4 +8,6 @@ $routes->group('dskpn', function ($routes) {
     $routes->get('mapping-static-field',      [Main::class,     'map_static']);
     $routes->get('tp-maintenance',            [Main::class,     'tp_maintenance'],              ['as' => 'tp_maintenance']);
     $routes->get('topic-list-in-cluster',     [Main::class,     'topic_list_in_cluster']);
+    $routes->get('list-registered-dskpn',     [Main::class,     'list_registered_dskpn']);
+    $routes->get('dskpn-view',                [Main::class,     'dskpn_view']);
 });
