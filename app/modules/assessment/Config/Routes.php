f9dab423c2d19548121496837e44cad0dfc3e9f2
MUHAMMAD HAZLAN SHAH BIN IDRIS <muhammadhazlanshahbinidris@gmail.com>
2024-05-21 00:46:06 +0800

    hazlan/overall_report

--- app/modules/assessment/Config/Routes.php
+++ app/modules/assessment/Config/Routes.php
@@ -3,6 +3,7 @@
 use App\Modules\Assessment\Controllers\Main;
 
 $routes->group('assessment', function ($routes) {
-    $routes->get('/',                   [Main::class,     'index'],     ['as' => 'dashboard']);
-    $routes->get('student-management',                   [Main::class,     'student_management']);
+    $routes->get('/',                       [Main::class,     'index'],     ['as' => 'dashboard']);
+    $routes->get('student-management',      [Main::class,     'student_management']);
+    $routes->get('overall-report',          [Main::class,     'overall_report']);
 });
