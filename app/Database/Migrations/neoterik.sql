82634272ded259ddadd0453dce8b2d14abe57fe9
MUHAMMAD HAZLAN SHAH BIN IDRIS <muhammadhazlanshahbinidris@gmail.com>
2024-05-22 12:08:00 +0800

    hazlan/fix_dashboard

--- app/Database/Migrations/neoterik.sql
+++ app/Database/Migrations/neoterik.sql
@@ -1,23 +1,27 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               5.1.72-community - MySQL Community Server (GPL)
--- Server OS:                    Win32
--- HeidiSQL Version:             10.2.0.5599
+-- Server version:               8.0.30 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.1.0.6537
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
 -- Dumping database structure for neoterik
-CREATE DATABASE IF NOT EXISTS `neoterik` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE IF NOT EXISTS `neoterik` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `neoterik`;
 
 -- Dumping structure for table neoterik.activity_assessment
 CREATE TABLE IF NOT EXISTS `activity_assessment` (
-  `aa_id` int(11) NOT NULL AUTO_INCREMENT,
+  `aa_id` int NOT NULL AUTO_INCREMENT,
   `aa_activity_desc` varchar(50) DEFAULT NULL,
   `aa_assessment_desc` varchar(50) DEFAULT NULL,
   `aa_is_parental_involved` enum('Y','N') DEFAULT NULL,
@@ -30,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `activity_assessment` (
 
 -- Dumping structure for table neoterik.cluster_main
 CREATE TABLE IF NOT EXISTS `cluster_main` (
-  `cm_id` int(11) NOT NULL AUTO_INCREMENT,
+  `cm_id` int NOT NULL AUTO_INCREMENT,
   `cm_code` varchar(50) DEFAULT '0',
   `cm_desc` varchar(50) DEFAULT '0',
   `created_at` datetime DEFAULT NULL,
@@ -41,17 +45,17 @@ CREATE TABLE IF NOT EXISTS `cluster_main` (
 
 -- Dumping data for table neoterik.cluster_main: 2 rows
 /*!40000 ALTER TABLE `cluster_main` DISABLE KEYS */;
-REPLACE INTO `cluster_main` (`cm_id`, `cm_code`, `cm_desc`, `created_at`, `updated_at`, `deleted_at`) VALUES
+INSERT INTO `cluster_main` (`cm_id`, `cm_code`, `cm_desc`, `created_at`, `updated_at`, `deleted_at`) VALUES
 	(1, 'A2000', 'Perkembangan Manusia', NULL, NULL, NULL),
 	(2, 'A2001', 'Kemandirian Spesis', NULL, NULL, NULL);
 /*!40000 ALTER TABLE `cluster_main` ENABLE KEYS */;
 
 -- Dumping structure for table neoterik.domain
 CREATE TABLE IF NOT EXISTS `domain` (
-  `d_id` int(11) NOT NULL AUTO_INCREMENT,
+  `d_id` int NOT NULL AUTO_INCREMENT,
   `d_name` varchar(50) DEFAULT NULL,
-  `gd_id` int(11) DEFAULT NULL,
-  `not_sureYet_id` int(11) DEFAULT NULL,
+  `gd_id` int DEFAULT NULL,
+  `not_sureYet_id` int DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
@@ -60,7 +64,7 @@ CREATE TABLE IF NOT EXISTS `domain` (
 
 -- Dumping data for table neoterik.domain: 23 rows
 /*!40000 ALTER TABLE `domain` DISABLE KEYS */;
-REPLACE INTO `domain` (`d_id`, `d_name`, `gd_id`, `not_sureYet_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
+INSERT INTO `domain` (`d_id`, `d_name`, `gd_id`, `not_sureYet_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
 	(32, '(DKM13) Kegigihan', 9, NULL, '2024-05-21 13:09:12', '2024-05-21 13:09:12', NULL),
 	(31, '(DKM12) Inisiatif', 9, NULL, '2024-05-21 13:09:12', '2024-05-21 13:09:12', NULL),
 	(30, '(DKM11) Inkuiri (Ik)', 9, NULL, '2024-05-21 13:09:12', '2024-05-21 13:09:12', NULL),
@@ -88,7 +92,7 @@ REPLACE INTO `domain` (`d_id`, `d_name`, `gd_id`, `not_sureYet_id`, `created_at`
 
 -- Dumping structure for table neoterik.domain_group
 CREATE TABLE IF NOT EXISTS `domain_group` (
-  `dg_id` int(11) NOT NULL AUTO_INCREMENT,
+  `dg_id` int NOT NULL AUTO_INCREMENT,
   `dg_title` varchar(50) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
@@ -98,7 +102,7 @@ CREATE TABLE IF NOT EXISTS `domain_group` (
 
 -- Dumping data for table neoterik.domain_group: 4 rows
 /*!40000 ALTER TABLE `domain_group` DISABLE KEYS */;
-REPLACE INTO `domain_group` (`dg_id`, `dg_title`, `created_at`, `updated_at`, `deleted_at`) VALUES
+INSERT INTO `domain_group` (`dg_id`, `dg_title`, `created_at`, `updated_at`, `deleted_at`) VALUES
 	(9, 'Kualiti Keperibadian', '2024-05-21 13:09:12', '2024-05-21 13:09:12', NULL),
 	(8, 'Kemandirian', '2024-05-21 13:09:12', '2024-05-21 13:09:12', NULL),
 	(7, 'Pengetahuan Asas', '2024-05-21 13:09:12', '2024-05-21 13:09:12', NULL),
@@ -107,10 +111,10 @@ REPLACE INTO `domain_group` (`dg_id`, `dg_title`, `created_at`, `updated_at`, `d
 
 -- Dumping structure for table neoterik.domain_mapping
 CREATE TABLE IF NOT EXISTS `domain_mapping` (
-  `dm_id` int(11) NOT NULL AUTO_INCREMENT,
+  `dm_id` int NOT NULL AUTO_INCREMENT,
   `dm_isChecked` enum('Y','N') DEFAULT NULL,
-  `d_id` int(11) DEFAULT NULL,
-  `ls_id` int(11) DEFAULT NULL,
+  `d_id` int DEFAULT NULL,
+  `ls_id` int DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
@@ -123,23 +127,28 @@ CREATE TABLE IF NOT EXISTS `domain_mapping` (
 
 -- Dumping structure for table neoterik.dskpn
 CREATE TABLE IF NOT EXISTS `dskpn` (
-  `dskpn_id` int(11) NOT NULL AUTO_INCREMENT,
-  `tm_id` int(11) DEFAULT '0',
-  `op_id` int(11) DEFAULT '0',
-  `aa_id` int(11) DEFAULT '0',
-  `created_by` datetime DEFAULT NULL,
-  `updated_by` datetime DEFAULT NULL,
-  `approved_by` datetime DEFAULT NULL,
+  `dskpn_id` int NOT NULL AUTO_INCREMENT,
+  `dskpn_theme` varchar(50) NOT NULL,
+  `dskpn_sub_theme` varchar(50) NOT NULL,
+  `tm_id` int NOT NULL DEFAULT '0',
+  `op_id` int DEFAULT '0',
+  `aa_id` int DEFAULT '0',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `approved_at` datetime DEFAULT NULL,
   PRIMARY KEY (`dskpn_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Dumping data for table neoterik.dskpn: 0 rows
+-- Dumping data for table neoterik.dskpn: 1 rows
 /*!40000 ALTER TABLE `dskpn` DISABLE KEYS */;
+INSERT INTO `dskpn` (`dskpn_id`, `dskpn_theme`, `dskpn_sub_theme`, `tm_id`, `op_id`, `aa_id`, `created_at`, `updated_at`, `deleted_at`, `approved_at`) VALUES
+	(1, '', '', 9, 14, NULL, '2024-05-22 02:34:15', '2024-05-22 02:34:15', NULL, NULL);
 /*!40000 ALTER TABLE `dskpn` ENABLE KEYS */;
 
 -- Dumping structure for table neoterik.learning_aid
 CREATE TABLE IF NOT EXISTS `learning_aid` (
-  `la_id` int(11) NOT NULL AUTO_INCREMENT,
+  `la_id` int NOT NULL AUTO_INCREMENT,
   `la_desc` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`la_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -150,108 +159,107 @@ CREATE TABLE IF NOT EXISTS `learning_aid` (
 
 -- Dumping structure for table neoterik.learning_standard
 CREATE TABLE IF NOT EXISTS `learning_standard` (
-  `ls_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ls_id` int NOT NULL AUTO_INCREMENT,
   `ls_details` varchar(50) DEFAULT '',
-  `sm_id` int(11) DEFAULT NULL,
-  `dskpn_id` int(11) DEFAULT NULL,
+  `sm_id` int DEFAULT NULL,
+  `dskpn_id` int DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`ls_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table neoterik.learning_standard: 2 rows
 /*!40000 ALTER TABLE `learning_standard` DISABLE KEYS */;
-REPLACE INTO `learning_standard` (`ls_id`, `ls_details`, `sm_id`, `dskpn_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(19, '3.2 Sains Gunaan\r\n3.3 Ethanol', 20, NULL, '2024-05-20 16:53:01', '2024-05-20 16:53:01', NULL),
-	(20, '1.1 Falsafah Kehidupan\r\n1.2 Kewarganegaraan', 21, NULL, '2024-05-20 16:53:01', '2024-05-20 16:53:01', NULL);
+INSERT INTO `learning_standard` (`ls_id`, `ls_details`, `sm_id`, `dskpn_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
+	(27, '2. Algebra', 28, 1, '2024-05-22 02:34:15', '2024-05-22 02:34:15', NULL),
+	(26, '1. Kejuruteraan', 27, 1, '2024-05-22 02:34:15', '2024-05-22 02:34:15', NULL);
 /*!40000 ALTER TABLE `learning_standard` ENABLE KEYS */;
 
 -- Dumping structure for table neoterik.objective_performance
 CREATE TABLE IF NOT EXISTS `objective_performance` (
-  `op_id` int(11) NOT NULL AUTO_INCREMENT,
+  `op_id` int NOT NULL AUTO_INCREMENT,
   `op_desc` varchar(50) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`op_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table neoterik.objective_performance: 1 rows
 /*!40000 ALTER TABLE `objective_performance` DISABLE KEYS */;
-REPLACE INTO `objective_performance` (`op_id`, `op_desc`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(9, 'Penggunaan Ethanol yang berhemat kepada masyarakat', '2024-05-20 16:52:59', '2024-05-20 16:52:59', NULL);
+INSERT INTO `objective_performance` (`op_id`, `op_desc`, `created_at`, `updated_at`, `deleted_at`) VALUES
+	(14, 'Kejuruteraan Algebra', '2024-05-22 02:34:13', '2024-05-22 02:34:13', NULL);
 /*!40000 ALTER TABLE `objective_performance` ENABLE KEYS */;
 
 -- Dumping structure for table neoterik.standard_performance
 CREATE TABLE IF NOT EXISTS `standard_performance` (
-  `sp_id` int(11) NOT NULL AUTO_INCREMENT,
-  `sp_tp_level` int(11) DEFAULT NULL,
+  `sp_id` int NOT NULL AUTO_INCREMENT,
+  `sp_tp_level` int DEFAULT NULL,
   `sp_tp_level_desc` varchar(50) DEFAULT NULL,
-  `sm_id` int(11) DEFAULT NULL,
-  `dskpn_id` int(11) DEFAULT NULL,
+  `sm_id` int DEFAULT NULL,
+  `dskpn_id` int DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`sp_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 
--- Dumping data for table neoterik.standard_performance: 7 rows
+-- Dumping data for table neoterik.standard_performance: 5 rows
 /*!40000 ALTER TABLE `standard_performance` DISABLE KEYS */;
-REPLACE INTO `standard_performance` (`sp_id`, `sp_tp_level`, `sp_tp_level_desc`, `sm_id`, `dskpn_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(11, 3, 'Membentuk Sebatian Ethanol Menggunakan Unsur Umum', 20, NULL, '2024-05-20 16:55:02', '2024-05-20 16:55:02', NULL),
-	(10, 2, 'Menyatakan bahaya Ethanol kepada Manusia', 20, NULL, '2024-05-20 16:55:02', '2024-05-20 16:55:02', NULL),
-	(9, 1, 'Menilai Kegunaan Sains dan Teknologi', 20, NULL, '2024-05-20 16:55:02', '2024-05-20 16:55:02', NULL),
-	(12, 4, 'Mengkaji Unsur Baru daripada Ethanol', 20, NULL, '2024-05-20 16:55:02', '2024-05-20 16:55:02', NULL),
-	(13, 1, 'Menilai Nilai murni budaya dan bangsa', 21, NULL, '2024-05-20 16:55:02', '2024-05-20 16:55:02', NULL),
-	(14, 2, 'Menyatakan nilai murni dan tauladan cerita rakyat', 21, NULL, '2024-05-20 16:55:02', '2024-05-20 16:55:02', NULL),
-	(15, 3, 'Membentuk peparduan dan kesepakatan didalam kelas', 21, NULL, '2024-05-20 16:55:02', '2024-05-20 16:55:02', NULL);
+INSERT INTO `standard_performance` (`sp_id`, `sp_tp_level`, `sp_tp_level_desc`, `sm_id`, `dskpn_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
+	(25, 2, 'Mencipta 2', 28, 1, '2024-05-22 02:48:44', '2024-05-22 02:48:44', NULL),
+	(24, 1, 'Mencipta 1', 28, 1, '2024-05-22 02:48:44', '2024-05-22 02:48:44', NULL),
+	(23, 3, 'Menilai 3', 27, 1, '2024-05-22 02:48:44', '2024-05-22 02:48:44', NULL),
+	(22, 2, 'Menilai 2', 27, 1, '2024-05-22 02:48:44', '2024-05-22 02:48:44', NULL),
+	(21, 1, 'Menilai 1', 27, 1, '2024-05-22 02:48:44', '2024-05-22 02:48:44', NULL);
 /*!40000 ALTER TABLE `standard_performance` ENABLE KEYS */;
 
 -- Dumping structure for table neoterik.subject_main
 CREATE TABLE IF NOT EXISTS `subject_main` (
-  `sm_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sm_id` int NOT NULL AUTO_INCREMENT,
   `sm_code` varchar(50) DEFAULT NULL,
   `sm_desc` varchar(50) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`sm_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table neoterik.subject_main: 2 rows
 /*!40000 ALTER TABLE `subject_main` DISABLE KEYS */;
-REPLACE INTO `subject_main` (`sm_id`, `sm_code`, `sm_desc`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(21, 'J5iDbJa', 'Sivik', '2024-05-20 16:53:01', '2024-05-20 16:53:01', NULL),
-	(20, '5a1TFWS', 'Sains', '2024-05-20 16:53:01', '2024-05-20 16:53:01', NULL);
+INSERT INTO `subject_main` (`sm_id`, `sm_code`, `sm_desc`, `created_at`, `updated_at`, `deleted_at`) VALUES
+	(28, 'nhG6iJw', 'Matematik', '2024-05-22 02:34:15', '2024-05-22 02:34:15', NULL),
+	(27, '7IfO8I0', 'Sains', '2024-05-22 02:34:15', '2024-05-22 02:34:15', NULL);
 /*!40000 ALTER TABLE `subject_main` ENABLE KEYS */;
 
 -- Dumping structure for table neoterik.topic_main
 CREATE TABLE IF NOT EXISTS `topic_main` (
-  `tm_id` int(11) NOT NULL AUTO_INCREMENT,
+  `tm_id` int NOT NULL AUTO_INCREMENT,
   `tm_desc` varchar(50) DEFAULT '0',
-  `tm_sub_theme` varchar(50) DEFAULT NULL,
-  `tm_theme` varchar(50) DEFAULT NULL,
   `tm_year` varchar(50) DEFAULT NULL,
-  `cm_id` int(11) DEFAULT NULL,
+  `cm_id` int DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`tm_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
--- Dumping data for table neoterik.topic_main: 7 rows
+-- Dumping data for table neoterik.topic_main: 8 rows
 /*!40000 ALTER TABLE `topic_main` DISABLE KEYS */;
-REPLACE INTO `topic_main` (`tm_id`, `tm_desc`, `tm_sub_theme`, `tm_theme`, `tm_year`, `cm_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(7, 'Jantung dan Pernafasan Manusia', 'tesa', 'asdada', '1313', 1, '2024-05-17 12:52:49', '2024-05-19 11:34:54', NULL),
-	(8, 'Pernafasan dan Respirasi', 'test', 'Individu', '4', 1, '2024-05-19 12:32:26', '2024-05-19 12:32:26', NULL),
-	(9, 'Tenaga ATP dan TDP, respirasi', 'test', 'Kumpulan', '4', 1, '2024-05-19 12:33:18', '2024-05-19 12:33:18', NULL),
-	(10, 'Bebola Mata dan Kanta', 'test', 'Individu', '3', 1, '2024-05-19 12:34:12', '2024-05-19 12:34:12', NULL),
-	(11, 'Pertumbuhan Spora', 'test', 'Individu', '3', 2, '2024-05-19 12:34:40', '2024-05-19 12:34:40', NULL),
-	(12, 'Belahan dedua', 'test', 'Kumpulan', '4', 2, '2024-05-19 12:35:47', '2024-05-19 12:35:47', NULL),
-	(13, 'Proses Kemenjadian Embryo', 'test', 'Individu', '4', 2, '2024-05-19 12:36:46', '2024-05-19 12:36:46', NULL);
+INSERT INTO `topic_main` (`tm_id`, `tm_desc`, `tm_year`, `cm_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
+	(7, 'Jantung dan Pernafasan Manusia', '1313', 1, '2024-05-17 12:52:49', '2024-05-19 11:34:54', NULL),
+	(14, 'Kehidupan selepas Meninggal', '4', 2, '2024-05-21 14:16:39', '2024-05-21 14:16:52', NULL),
+	(8, 'Pernafasan dan Respirasi', '4', 1, '2024-05-19 12:32:26', '2024-05-19 12:32:26', NULL),
+	(9, 'Tenaga ATP dan TDP, respirasi', '4', 1, '2024-05-19 12:33:18', '2024-05-19 12:33:18', NULL),
+	(10, 'Bebola Mata dan Kanta', '3', 1, '2024-05-19 12:34:12', '2024-05-19 12:34:12', NULL),
+	(11, 'Pertumbuhan Spora', '3', 2, '2024-05-19 12:34:40', '2024-05-21 14:17:12', NULL),
+	(12, 'Belahan dedua', '4', 2, '2024-05-19 12:35:47', '2024-05-21 14:17:30', NULL),
+	(13, 'Proses Kemenjadian Embryo', '4', 2, '2024-05-19 12:36:46', '2024-05-21 14:17:14', NULL);
 /*!40000 ALTER TABLE `topic_main` ENABLE KEYS */;
 
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
