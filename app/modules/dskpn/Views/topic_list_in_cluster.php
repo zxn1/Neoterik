72e6c3a170670c0f94cba3d8302c41a31948098e
Muhd Jamil Husni <maintanance1991@gmail.com>
2024-06-02 05:05:03 +0800

    zues style

--- app/modules/dskpn/Views/topic_list_in_cluster.php
+++ app/modules/dskpn/Views/topic_list_in_cluster.php
@@ -23,6 +23,8 @@
         border-bottom-right-radius: 0;
     }
 
+
+
     .select2-container--open .select2-dropdown--below {
         border-top: none;
         border-top-left-radius: 0;
@@ -30,9 +32,12 @@
         border: 1px solid #d2d6da;
     }
 
+
+
     .select2-container--default .select2-selection--single .select2-selection__rendered {
         /* color: #444; */
         line-height: inherit !important;
+
     }
 
     .select2-container--default .select2-selection--single {
@@ -40,6 +45,8 @@
         border: 0 !important;
         /* border-radius: 4px; */
     }
+
+
 
     /* CSS for desktop screens */
     .list-group {
@@ -82,33 +89,27 @@
         border-bottom-left-radius: 15px !important;
     }
 
-    /* .row>* {
-    padding-right: calc(var(--bs-gutter-x)* 0);
-    padding-left: calc(var(--bs-gutter-x)* 0);
-    margin-right: calc(var(--bs-gutter-x)* .5);
-    margin-left: calc(var(--bs-gutter-x)* .5);
-  } */
-
-    .pagination-container {
+  .pagination-container {
         display: flex;
-        justify-content: center;
-        align-items: center;
+        flex-direction: column;
+        justify-content: flex-start; /* Align items to the left */
+        align-items: flex-start;
         margin-top: 20px;
     }
 
     .pagination-button {
-        background-color: #e9ecef;
+        background-color: rgba(233, 236, 239, 0.2); /* Background color with 50% opacity */
         border: none;
         color: #6c757d;
         padding: 8px 12px;
-        margin: 0 2px;
+        margin: 2px 0; /* Add vertical margin between buttons */
         text-decoration: none;
         border-radius: 5px;
         cursor: pointer;
     }
 
     .pagination-button.active {
-        background-color: #888a88;
+        background-color: #cb0c9f;
         color: white;
         font-weight: bold;
     }
@@ -210,70 +211,72 @@
         <div class="card-header d-flex p-3 bg-gradient-primary">
             <h6 class="my-auto text-white">Senarai Topik Dalam Kluster</h6>
         </div>
-        <div class="card-body p-3">
-            <?php if ($hasData) : ?>
-                <div class="accordion" id="accordionRental">
-                    <?php foreach ($cluster as $clust) { ?>
-                        <div class="accordion-item mb-3">
-                            <h5 class="accordion-header" id="headingOne">
-                                <button class="accordion-button border-bottom font-weight-bold collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?= $clust['cm_code']; ?>" aria-expanded="false" aria-controls="collapse<?= $clust['cm_code']; ?>">
-                                    <i class="collapse-close fa fa-plus text-xs pt-1 position-absolute end-0 me-3" aria-hidden="true"></i>
-                                    <i class="collapse-open fa fa-minus text-xs pt-1 position-absolute end-0 me-3" aria-hidden="true"></i>
-                                    <?= $clust['cm_desc']; ?>
-                                </button>
-                            </h5>
-                            <div id="collapse<?= $clust['cm_code']; ?>" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionRental">
-                                <div class="accordion-body custom row" id="tahap-penguasaan">
-                                    <?php foreach ($topik_main as $topik) {
-                                        if ($topik['cm_id'] == $clust['cm_id']) { ?>
-                                            <div class="col-md-12">
-                                                <div class="d-flex flex-column h-100">
-                                                    <div id="collection1-<?= $topik['tm_id']; ?>">
-                                                        <div class="d-flex w-100 align-items-center mb-2" id="1-collection1-<?= $topik['tm_id']; ?>">
-                                                            <span class="form-control me-2"><?= $topik['tm_desc']; ?></span>
-                                                            <a class="btn btn-link text-info text-gradient px-1 mb-0" href="<?= route_to('dskpn_by_topic', $topik['tm_id']) ?>">
-                                                                <i class="far fa-eye fa-lg me-2" aria-hidden="true"></i>
-                                                            </a>
-                                                            <a class="btn btn-link text-danger text-gradient px-1 mb-0" href="javascript:void(0)" onclick="$('#1-collection1-<?= $topik['tm_id']; ?>').remove(); deleteTopic(<?= $topik['tm_id']; ?>);">
-                                                                <i class="far fa-trash-alt fa-lg me-2" aria-hidden="true"></i>
-                                                            </a>
+        <div class="container">
+
+            <div class="row">
+                <div class="col-sm-2">
+                    <div class="pagination-container">
+                    <p class="text-center mb-2"><strong style="font-size: 1.0rem; color: black;">Pilih Tahap</strong></p>
+                        <?php foreach ($years as $yearItem) : ?>
+                            <?php if ($yearItem == $selectedYear) : ?>
+                                <span class="pagination-button active">Tahap <?= $yearItem; ?></span>
+                            <?php else : ?>
+                                <a href="?year=<?= $yearItem; ?>" class="pagination-button">Tahap <?= $yearItem; ?></a>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+
+                <div class="col-sm">
+                    <?php if ($hasData) : ?>
+                    <div class="accordion" id="accordionRental">
+                        <br>
+                    <p class="text-center"><strong style="font-size: 1.0rem; color: black;">Senarai Topik</strong></p>
+                        <?php foreach ($cluster as $clust) { ?>
+                            <div class="accordion-item mb-3">
+                                <h5 class="accordion-header" id="headingOne">
+                                    <button class="accordion-button border-bottom font-weight-bold collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?= $clust['cm_code']; ?>" aria-expanded="false" aria-controls="collapse<?= $clust['cm_code']; ?>">
+                                        <i class="collapse-close fa fa-plus text-xs pt-1 position-absolute end-0 me-3" aria-hidden="true"></i>
+                                        <i class="collapse-open fa fa-minus text-xs pt-1 position-absolute end-0 me-3" aria-hidden="true"></i>
+                                        <?= $clust['cm_desc']; ?>
+                                    </button>
+                                </h5>
+                                <div id="collapse<?= $clust['cm_code']; ?>" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionRental">
+                                    <div class="accordion-body custom row" id="tahap-penguasaan">
+                                        <?php foreach ($topik_main as $topik) {
+                                            if ($topik['cm_id'] == $clust['cm_id']) { ?>
+                                                <div class="col-md-12">
+                                                    <div class="d-flex flex-column h-100">
+                                                        <div id="collection1-<?= $topik['tm_id']; ?>">
+                                                            <div class="d-flex w-100 align-items-center mb-2" id="1-collection1-<?= $topik['tm_id']; ?>">
+                                                                <span class="form-control me-2"><?= $topik['tm_desc']; ?></span>
+                                                                <a class="btn btn-link text-info text-gradient px-1 mb-0" href="<?= route_to('dskpn_by_topic', $topik['tm_id']) ?>">
+                                                                    <i class="far fa-eye fa-lg me-2" aria-hidden="true"></i>
+                                                                </a>
+                                                                <a class="btn btn-link text-danger text-gradient px-1 mb-0" href="javascript:void(0)" onclick="$('#1-collection1-<?= $topik['tm_id']; ?>').remove(); deleteTopic(<?= $topik['tm_id']; ?>);">
+                                                                    <i class="far fa-trash-alt fa-lg me-2" aria-hidden="true"></i>
+                                                                </a>
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                    <?php }
-                                    } ?>
+                                        <?php }
+
+                                        } ?>
+                                    </div>
                                 </div>
                             </div>
+                        <?php } ?>
+                    </div>
+                    <?php else : ?>
+                        <div class="text-center py-4">
+                        <br><br><br>
+                            <strong>Tiada data yang didaftarkan bagi tahun yang dipilih</strong>
                         </div>
-                    <?php } ?>
-                </div>
-            <?php else : ?>
-                <div class="text-center py-4">
-                    <strong>Tiada data yang didaftarkan bagi tahun yang dipilih</strong>
-                </div>
-            <?php endif; ?>
-
-            <div class="card-body p-3">
-                <div class="pagination-container">
-                    <?php if ($selectedYear > 1) : ?>
-                        <a href="?year=<?= $selectedYear - 1; ?>" class="pagination-button">&laquo;</a>
-                    <?php endif; ?>
-
-                    <?php foreach ($years as $yearItem) : ?>
-                        <?php if ($yearItem == $selectedYear) : ?>
-                            <span class="pagination-button active"><?= $yearItem; ?></span>
-                        <?php else : ?>
-                            <a href="?year=<?= $yearItem; ?>" class="pagination-button"><?= $yearItem; ?></a>
-                        <?php endif; ?>
-                    <?php endforeach; ?>
-
-                    <?php if ($selectedYear < 6) : ?>
-                        <a href="?year=<?= $selectedYear + 1; ?>" class="pagination-button"> &raquo;</a>
                     <?php endif; ?>
                 </div>
             </div>
-
+            <br>
         </div>
     </div>
 </div>
