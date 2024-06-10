2b542beccd4976fa490024fc20e58adaf9516b8d
Jamil Husni Abdul Hanif <100312@corp.b2be.com>
2024-06-10 22:12:58 +0800

    ubah naming dan database reset

--- app/Views/render/main/sidebar.php
+++ app/Views/render/main/sidebar.php
@@ -26,7 +26,7 @@
               </g>
             </svg>
           </div>
-          <span class="nav-link-text ms-1">Papan Pemuka</span>
+          <span class="nav-link-text ms-1">Utama</span>
         </a>
       </li>
       <li class="nav-item">
