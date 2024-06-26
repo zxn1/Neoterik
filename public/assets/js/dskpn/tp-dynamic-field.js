c1ae6da3acd4eaf3a98cbb79e8df6699299f13b0
MUHAMMAD HAZLAN SHAH BIN IDRIS <muhammadhazlanshahbinidris@gmail.com>
2024-06-26 23:39:03 +0800

    hazlan/backlog_45

--- public/assets/js/dskpn/tp-dynamic-field.js
+++ public/assets/js/dskpn/tp-dynamic-field.js
@@ -24,7 +24,11 @@ $(document).ready(function() {
                     </div>
                 </div>
                 <div class="p-2 pb-3">
-                    <span class="btn bg-gradient-primary mt-2" onclick="addField('${item.sm_code}')">Tambah TP</span>
+                    <span class="btn bg-gradient-primary mt-2" onclick="addField('${item.sm_code}')">Tambah TP &nbsp;&nbsp;
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"></path>
+                        </svg>
+                    </span>
                 </div>
             </ul>
         `);
