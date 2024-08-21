de1fdef1d794a66ed221d9ebb92317052b1d1d3c
Muhd Jamil Husni <maintanance1991@gmail.com>
2024-08-22 01:06:25 +0800

    boleh dah insya-Allah

--- app/modules/dskpn/Views/mapping_assessment_and_activity.php
+++ app/modules/dskpn/Views/mapping_assessment_and_activity.php
@@ -79,7 +79,7 @@
           <label for="kluster">KLUSTER</label>
           <select class="form-control select2" id="kluster" name="kluster" <?= isset($topikncluster) ? 'disabled' : ''; ?>>
             <?php if (isset($topikncluster)) { ?>
-              <option value="<?= $topikncluster['cm_id']; ?>" selected><?= $topikncluster['cm_desc']; ?></option>
+              <option value="<?= $topikncluster['ctm_id']; ?>" selected><?= $topikncluster['ctm_desc']; ?></option>
             <?php } else { ?>
               <option value="AL">Alabama</option>
               <option value="WY">Wyoming</option>
@@ -108,8 +108,30 @@
         <div class="card-header d-flex justify-content-between align-items-center p-3 bg-gradient-primary">
           <h6 class="my-auto text-white">Idea Pengajaran (Aktiviti)</h6>
         </div>
-
-        <textarea id="editor-idea-pengajaran" rows="9" name="idea-pengajaran" class="multisteps-form__textarea form-control zero-top-border"><?= $act_assess_idea_pengajaran; ?></textarea>
+        <div id="teaching-idea-and-activity" class="row p-3">
+          <?php for($i = 0; $i < 3; $i++) { ?>
+          <div class="row m-1" id="activity-idea-item-<?= $i; ?>">
+            <div class="col-2 p-0 pe-1">
+              <input type="number" name="activity-idea-number[]" step="0.01" min="0" class="form-control p-1" placeholder="1.1" style="height : 45px;">
+            </div>
+            <div class="col-10 d-flex p-0" style="margin-bottom : 5px;">
+              <input type="text" class="form-control p-1 me-1" name="activity-idea-input[]" placeholder="Idea pengajaran bagi topik DSKPN ini" style="height : 45px;">
+              <div class="input-group-prepend me-1" style="margin-right : 5px;" onclick="$('#activity-idea-item-<?= $i; ?>').remove();">
+                  <button class="input-group-text justify-content-center" id="btnGroupAddon" style="height : 45px; width : 50px;">
+                      <i class="fas fa-trash-alt" style="color:red;"></i>
+                  </button>
+              </div>
+            </div>
+          </div>
+          <?php } ?>
+        </div>
+        <div class="p-3 pt-0 pb-2">
+          <span class="btn bg-gradient-primary mt-1" onclick="addActivityItemField('method-instruction-ID', 'ID')">Tambah Aktiviti &nbsp;&nbsp;
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"></path>
+            </svg>
+          </span>
+        </div>
       </div>
     </div>
   </div>
@@ -120,13 +142,39 @@
         <div class="card-header d-flex justify-content-between align-items-center p-3 bg-gradient-primary">
           <h6 class="my-auto text-white">Pentaksiran</h6>
         </div>
-        <textarea id="editor-pentaksiran" rows="9" name="pentaksiran" class="multisteps-form__textarea form-control zero-top-border"><?= $act_assess_pentaksiran; ?></textarea>
+        <?php foreach($assessment_category as $index => $category) { ?>
+        <div id="assessment-part-<?= $category['asc_id']; ?>" class="row p-2">
+          <h6 class="m-1"><?= ($index + 1) . ". " . $category['asc_desc']; ?></h6>
+          <div class="p-0 pe-3 ps-3" id="assessment-div-<?= $category['asc_id']; ?>">
+            <div class="row m-1" id="assessment-<?= $category['asc_id']; ?>-item-0">
+              <div class="col-2 p-0 pe-1">
+                <input type="number" name="assessment-number[<?= $category['asc_id']; ?>][]" step="0.01" min="0" class="form-control p-1" placeholder="1.1" style="height : 45px;">
+              </div>
+              <div class="col-10 d-flex p-0" style="margin-bottom : 5px;">
+                <input type="text" class="form-control p-1 me-1" name="assessment-input[<?= $category['asc_id']; ?>][]" placeholder="Idea pentaksiran bagi <?= strtolower($category['asc_desc']); ?>" style="height : 45px;">
+                <div class="input-group-prepend me-1" style="margin-right : 5px;" onclick="$('#assessment-<?= $category['asc_id']; ?>-item-0').remove();">
+                    <button class="input-group-text justify-content-center" id="btnGroupAddon" style="height : 45px; width : 50px;">
+                        <i class="fas fa-trash-alt" style="color:red;"></i>
+                    </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="p-3 pt-0 pb-1">
+            <span class="btn bg-gradient-primary mt-1" onclick="addAsessmentItemField('<?= $category['asc_id']; ?>')">Tambah Item <?= $category['asc_desc']; ?>&nbsp;&nbsp;
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"></path>
+              </svg>
+            </span>
+          </div>
+        </div>
+        <?php } ?>
       </div>
     </div>
   </div>
 
   <div class="row">
-    <div class="col-lg-5">
+    <div class="col-lg-6">
       <div class="card">
         <div class="card-header d-flex p-3 bg-gradient-primary">
           <h6 class="my-auto text-white">Alat Bantu Mengajar (ABM)</h6>
@@ -185,18 +233,26 @@
     </div>
 
     <div class="col-lg-4">
-      <table class="mt-2 card" style="border-style : solid; border-width : 2px;">
-        <tr>
-          <td>
-            <div class="form-check form-switch mb-0 d-flex align-items-center justify-content-center">
-              <input class="form-check-input" value="Y" name="parent-involvement" type="checkbox" id="flexSwitchCheckDefault11" <?= (!empty($act_assess_parent_involve) && $act_assess_parent_involve == 'Y') ? 'checked' : ''; ?>>
-            </div>
-          </td>
-          <td>
-            Keperluan Pengilabatan Ibu Bapa
-          </td>
-        </tr>
-      </table>
+
+      <div class="card">
+        <div class="card-header d-flex p-3 bg-gradient-primary">
+          <h6 class="my-auto text-white">Keperluan Pengilabatan Ibu Bapa</h6>
+        </div>
+        <div class="card-body">
+          <table class="mt-2 card" style="border-style : solid; border-width : 2px;">
+            <tr>
+              <td>
+                <div class="form-check form-switch mb-0 d-flex align-items-center justify-content-center">
+                  <input class="form-check-input" value="Y" name="parent-involvement" type="checkbox" id="flexSwitchCheckDefault11" <?= (!empty($act_assess_parent_involve) && $act_assess_parent_involve == 'Y') ? 'checked' : ''; ?>>
+                </div>
+              </td>
+              <td>
+                Ya, melibatkan ibu-bapa
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
 
     </div>
   </div>
