f8e6e4e7bfd82e8f5bd1e6f2a317454d5d40f392
MUHAMMAD HAZLAN SHAH BIN IDRIS <muhammadhazlanshahbinidris@gmail.com>
2024-09-03 21:59:17 +0800

    hazlan/update_login_ui

--- app/Views/render/main/sidebar.php
+++ app/Views/render/main/sidebar.php
@@ -42,6 +42,7 @@
           </a>
         </li>
       <?php endif; ?>
+
       <?php if (get_user_role() == $both_roles[1]) : ?>
         <?php
         $in_main_configuration = url_is(route_to('view_subject')) || url_is(route_to('view_cluster')) || url_is(route_to('view_topic')) || url_is(route_to('view_teacher_cluster')) || url_is(route_to('view_tp_core_setup')) || url_is(route_to('view_core_competency_setup')) || url_is(route_to('view_standard_performance')) || url_is(route_to('view_core_competency_list'));
@@ -120,6 +121,48 @@
           </div>
         </li>
       <?php endif; ?>
+
+      <!-- Super Admin -->
+      <?php
+      $in_main_configuration = url_is(route_to('view_user_access'));
+      ?>
+      <li class="nav-item">
+        <a data-bs-toggle="collapse" href="#admin-setting" class="nav-link " aria-controls="admin-setting" role="button" aria-expanded="<?= $in_main_configuration ? 'true' : 'false' ?>">
+          <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center d-flex align-items-center justify-content-center  me-2">
+            <svg class="text-dark" width="16px" height="16px" viewBox="0 0 40 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+              <title>settings</title>
+              <g id="Basic-Elements" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <g id="Rounded-Icons" transform="translate(-2020.000000, -442.000000)" fill="#FFFFFF" fill-rule="nonzero">
+                  <g id="Icons-with-opacity" transform="translate(1716.000000, 291.000000)">
+                    <g id="settings" transform="translate(304.000000, 151.000000)">
+                      <polygon class="color-background" id="Path" opacity="0.596981957" points="18.0883333 15.7316667 11.1783333 8.82166667 13.3333333 6.66666667 6.66666667 0 0 6.66666667 6.66666667 13.3333333 8.82166667 11.1783333 15.315 17.6716667"></polygon>
+                      <path class="color-background" d="M31.5666667,23.2333333 C31.0516667,23.2933333 30.53,23.3333333 30,23.3333333 C29.4916667,23.3333333 28.9866667,23.3033333 28.48,23.245 L22.4116667,30.7433333 L29.9416667,38.2733333 C32.2433333,40.575 35.9733333,40.575 38.275,38.2733333 L38.275,38.2733333 C40.5766667,35.9716667 40.5766667,32.2416667 38.275,29.94 L31.5666667,23.2333333 Z" id="Path" opacity="0.596981957"></path>
+                      <path class="color-background" d="M33.785,11.285 L28.715,6.215 L34.0616667,0.868333333 C32.82,0.315 31.4483333,0 30,0 C24.4766667,0 20,4.47666667 20,10 C20,10.99 20.1483333,11.9433333 20.4166667,12.8466667 L2.435,27.3966667 C0.95,28.7083333 0.0633333333,30.595 0.00333333333,32.5733333 C-0.0583333333,34.5533333 0.71,36.4916667 2.11,37.89 C3.47,39.2516667 5.27833333,40 7.20166667,40 C9.26666667,40 11.2366667,39.1133333 12.6033333,37.565 L27.1533333,19.5833333 C28.0566667,19.8516667 29.01,20 30,20 C35.5233333,20 40,15.5233333 40,10 C40,8.55166667 39.685,7.18 39.1316667,5.93666667 L33.785,11.285 Z" id="Path"></path>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </svg>
+
+
+          </div>
+          <span class="nav-link-text ms-1">Tetapan Admin</span>
+        </a>
+        <div class="collapse <?= $in_main_configuration ? 'show' : ''; ?>" id="admin-setting">
+          <ul class="nav ms-1 ps-1">
+            <li class="nav-item ">
+              <a class="nav-link  <?= (url_is(route_to('view_user_access'))) ? 'active' : ''; ?>" href="<?= route_to('view_user_access'); ?>" style="overflow : hidden;">
+                <div class="icon icon-shape icon-sm shadow border-radius-md text-center d-flex align-items-center justify-content-center">
+                  <i class="fas fa-user-tie" style="color : #3c4242; height : 16px; width : 16px;"></i>
+                </div>
+                <span class="nav-link-text ms-1">Akses Pengguna</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <!-- End Super Admin -->
+
       <?php if (get_user_role() == $both_roles[1]) : ?>
         <li class="nav-item">
           <a class="nav-link <?= (url_is(route_to('cluster_topic'))) ? 'active' : ''; ?>" href="<?= route_to('cluster_topic'); ?>">
