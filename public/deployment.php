e66962aefe86520d8aeaf3ec65c278f329ae68c9
Zxn1 <maintanance1991@gmail.com>
2025-05-09 09:51:37 +0800

    fix deployment

--- public/deployment.php
+++ public/deployment.php
@@ -1,5 +1,5 @@
 <?php
-$currentTag = trim(exec('GIT_SSH_COMMAND="ssh -i ../../neoteric_deployment_key -o IdentitiesOnly=yes" git describe --tags'));
+$currentTag = trim(exec('GIT_SSH_COMMAND="ssh -i ../neoteric_deployment_key -o IdentitiesOnly=yes" git describe --tags'));
 
 print_r("current version: " . $currentTag . "<br><br>");
 $gitUpdateLog = trim(exec('git checkout master && git reset --hard origin/master && git pull'));
