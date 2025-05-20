fddd252c7055caecd18637bfa557d160279cd8a8
Zxn1 <maintanance1991@gmail.com>
2025-05-20 22:55:23 +0800

    update column that need to be update to text

--- app/Database/Migrations/neoterik_varchar_to_text.sql
+++ app/Database/Migrations/neoterik_varchar_to_text.sql
@@ -3,3 +3,9 @@ ALTER TABLE `learning_standard_item`
 
 ALTER TABLE `standard_performance`
 	CHANGE COLUMN `sp_tp_level_desc` `sp_tp_level_desc` TEXT NULL COLLATE 'utf8mb4_general_ci' AFTER `sp_tp_level`;
+
+ALTER TABLE `learning_aid`
+	CHANGE COLUMN `la_desc` `la_desc` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci' AFTER `la_id`;
+
+ALTER TABLE `objective_performance`
+	CHANGE COLUMN `opm_desc` `opm_desc` TEXT NULL COLLATE 'utf8mb4_general_ci' AFTER `opm_number`;
